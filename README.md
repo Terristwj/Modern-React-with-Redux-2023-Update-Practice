a7ddc984fc4df64f15f154dd1faab77ad63573c6
Terris <27739101+Terristwj@users.noreply.github.com>
2023-05-11 03:31:00 +0800

    Initialized project

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<pre><code>npm install -g cra-template-typescript
+npm install -g create-react-app
+create-react-app project-name --template typescript</code></pre>
 
 ## Available Scripts
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Step 1) Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// Step 2) Get a reference to the div with ID root
+const element = document.getElementById("root") as HTMLElement;
+
+// Step 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(element);
+
+// Step 4) Create a component
+function App() {
+  const msg: string = "Hello there!";
+  return (
+    <div>
+      <h1>{msg}</h1>
+      <textarea autoFocus />
+    </div>
+  );
+}
+
+// Step 5) Show the component on the screen
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
